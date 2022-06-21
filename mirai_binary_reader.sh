968f41e29bdfad02e0708b546689d50cf216f7fd
kokubu <veyron362@gmail.com>
2022-06-21 15:20:59 +0900

    no message

--- mirai_binary_reader.sh
+++ mirai_binary_reader.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-start_time=`date "+%Y-%m-%d %H:%M:%S"`
+start_time_1=`date "+%Y-%m-%d %H:%M:%S"`
 
 python binary_reader.py
 
-end_time=`date "+%Y-%m-%d %H:%M:%S"`
+end_time_1=`date "+%Y-%m-%d %H:%M:%S"`
 
-echo $start_time
-echo $end_time
+sleep 5
+
+start_time_2=`date "+%Y-%m-%d %H:%M:%S"`
+
+python binary_reader.py
+
+end_time_2=`date "+%Y-%m-%d %H:%M:%S"`
+
+sleep 5
+
+start_time_3=`date "+%Y-%m-%d %H:%M:%S"`
+
+python binary_reader.py
+
+end_time_3=`date "+%Y-%m-%d %H:%M:%S"`
+
+echo $start_time_1
+echo $end_time_1
+echo $start_time_2
+echo $end_time_2
+echo $start_time_3
+echo $end_time_3
