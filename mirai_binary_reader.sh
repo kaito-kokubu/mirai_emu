d0197375609bcf75fb4967fd349b6bf012786294
kokubu <veyron362@gmail.com>
2022-07-11 09:45:57 +0900

    no message

--- mirai_binary_reader.sh
+++ mirai_binary_reader.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-start_time_1=`date "+%Y-%m-%d %H:%M:%S"`
+start_time_1=`date "+%Y-%m-%d %H:%M:%S.%6N"`
 
 python binary_reader.py
 
-end_time_1=`date "+%Y-%m-%d %H:%M:%S"`
+end_time_1=`date "+%Y-%m-%d %H:%M:%S.%6N"`
 
-sleep 5
+#sleep 5
 
-start_time_2=`date "+%Y-%m-%d %H:%M:%S"`
+#start_time_2=`date "+%Y-%m-%d %H:%M:%S"`
 
-python binary_reader.py
+#python binary_reader.py
 
-end_time_2=`date "+%Y-%m-%d %H:%M:%S"`
+#end_time_2=`date "+%Y-%m-%d %H:%M:%S"`
 
-sleep 5
+#sleep 5
 
-start_time_3=`date "+%Y-%m-%d %H:%M:%S"`
+#start_time_3=`date "+%Y-%m-%d %H:%M:%S"`
 
-python binary_reader.py
+#python binary_reader.py
 
-end_time_3=`date "+%Y-%m-%d %H:%M:%S"`
+#end_time_3=`date "+%Y-%m-%d %H:%M:%S"`
 
 echo $start_time_1
 echo $end_time_1
-echo $start_time_2
-echo $end_time_2
-echo $start_time_3
-echo $end_time_3
+#echo $start_time_2
+#echo $end_time_2
+#echo $start_time_3
+#echo $end_time_3
