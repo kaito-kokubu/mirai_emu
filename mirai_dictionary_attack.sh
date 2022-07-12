ce32e29fe77a520ff266afd71386838cf1232786
kokubu <veyron362@gmail.com>
2022-07-13 03:22:03 +0900

    no message

--- mirai_dictionary_attack.sh
+++ mirai_dictionary_attack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-start_time=`date "+%Y-%m-%d %H:%M:%S"`
+start_time=`date "+%Y-%m-%d %H:%M:%S.%6N"`
 
 python dictionary_attack.py
 
-end_time=`date "+%Y-%m-%d %H:%M:%S"`
+end_time=`date "+%Y-%m-%d %H:%M:%S.%6N"`
 
 echo $start_time
 echo $end_time
